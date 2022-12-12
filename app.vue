87f4a9426f5f3f9744204310363e6465335818d6
nasubi916 <r916nis@gmail.com>
2022-12-12 18:37:42 +0900

    山札　作成

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    VegeVanguard!!
+    VegeVanguard!!<br>
+    composablesテスト:{{ hello }}<br>
+    オブジェクト配列による山札の作成:{{ cardList }}<br>
+    見やすくする:
+    <div v-for="card in cardList">
+      <ul>
+        <li>
+          name:{{ card.name }} {{ 'atk:' + card.atk }} {{ 'def:' + card.def }} 満腹度: {{ card.hungry }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+const hello = useTest();
+const cardList = useCards();
+</script>
